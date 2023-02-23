1767face2fba85c5d812290d229ae0f4925638aa
Giseudo <giseudo@gmail.com>
2023-02-22 23:25:59 -0300

    Updated heart

--- Assets/Game/Scripts/UI/UIHealth.cs
+++ Assets/Game/Scripts/UI/UIHealth.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Game.Core;
 using Game.Combat;
 using TMPro;
@@ -8,12 +9,25 @@ public class UIHealth : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI _text;
 
+    [SerializeField]
+    private RectMask2D _heartMask;
+
+    private RectTransform _rect;
     private Damageable _damageable;
 
-    public void OnHurt(Damageable damageable) => UpdateText();
+    public float Height => _heartMask.rectTransform.rect.height;
+
+    public void OnHurt(Damageable damageable)
+    {
+        float value = Height * (1f - ((float)damageable.Health / (float)damageable.MaxHealth));
+
+        _heartMask.padding = new Vector4(0f, 0f, 0f, value);
+    }
 
     public void Start()
     {
+        TryGetComponent<RectTransform>(out _rect);
+
         GameManager.MainTree.TryGetComponent<Damageable>(out _damageable);
 
         if (_damageable == null) return;
